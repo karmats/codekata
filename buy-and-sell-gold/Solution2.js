6bc983819093e1db80ee7ad5d71c548e6275d40b
Mats Roshauw <karmats@gmail.com>
2017-05-17 22:15:00 +0200

    Alternate solution, renamed variables

--- buy-and-sell-gold/Solution2.js
+++ buy-and-sell-gold/Solution2.js
@@ -7,39 +7,28 @@ function Solution() {
     if (numDays <= 0) {
         throw new Error('Must be at least one day');
     }
-    const sells = [];
-    for (let i = 0; i < numDays; i++) {
-        const priceOnDay = api.getPriceOnDay(i);
-        if (!priceOnDay) {
-            throw new Error('Price on day must be defined');
-        }
-        sells[i] = api.getPriceOnDay(i);
+    this.bestResult = {
+        buyDay: -1,
+        sellDay: -1,
+        profit: -1
     }
 
     // Calculate the best result buy going through the whole array with gold price data
     // and check which buy day that gives the largest profit with sell day
-    this.bestResult = sells
-    .map((sell, buyDay) => {
-        let bestDiff = {
-            buyDay: buyDay,
-            sellDay: 0,
-            profit: 0
-        }
-        for (let sellDay = buyDay; sellDay < sells.length; sellDay++) {
-            const profit = sells[sellDay] - sell;
-            if (profit > bestDiff.profit) {
-                bestDiff.sellDay = sellDay;
-                bestDiff.profit = profit;
+    for (let buyDay = 0; buyDay < numDays; buyDay++) {
+        const buyPrice = api.getPriceOnDay(buyDay);
+        for (let sellDay = buyDay; sellDay < numDays; sellDay++) {
+            const sellPrice = api.getPriceOnDay(sellDay);
+            const profit = sellPrice - buyPrice;
+            if (profit > this.bestResult.profit) {
+                this.bestResult = {
+                    buyDay: buyDay,
+                    sellDay: sellDay,
+                    profit: profit
+                }
             }
         }
-        return bestDiff;
-    })
-    .reduce((p, c) => {
-        if (c.profit > p.profit) {
-            return c;
-        }
-        return p;
-    }, { profit: -1 })
+    }
 }
 
 /**
